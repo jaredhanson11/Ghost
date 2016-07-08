effd2a5f1e4376391ed799fd96a7bad2798b7a06
John Clarke <johnclarke1996@gmail.com>
2016-07-08 11:07:21 -0400

    userdata

--- ghost/ghost/MainTableViewController.swift
+++ ghost/ghost/MainTableViewController.swift
@@ -39,6 +39,7 @@ class MainTableViewController: UITableViewController {
         // TODO: SAVE MESSAGES TO CACHE
         
         
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
